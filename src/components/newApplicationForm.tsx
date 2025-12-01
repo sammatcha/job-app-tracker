866810a092fa79d9a937f1d60abc5741d11a8702
Samantha <samantha@uppointinc.com>
2025-12-01 11:28:43 -0800

    dashboard

---
+++ src/components/newApplicationForm.tsx
@@ -0,0 +1,126 @@
+import { useState } from "react"
+
+interface JobApplication {
+ id: number;
+    company: string;
+    location: string;
+    position: string;
+    salary: string;
+    status: string;
+    appliedDate: string;
+}
+interface newAppFormProps {
+    onSubmit: (newApp:JobApplication) => void
+    onCancel: () => void
+}
+
+export default function NewAppForm(props:newAppFormProps){
+    const [company, setCompany] = useState("");
+    const [location, setLocation] = useState("");
+    const [position, setPosition] = useState("");
+    const [salary, setSalary] = useState("");
+    const [status, setStatus] = useState("Applied");
+
+   const handleSubmit = () => {
+
+    const newApp = {
+        id: Date.now(),
+        company,
+        location,
+        position,
+        salary,
+        status,
+        appliedDate: new Date().toLocaleDateString('en')
+    }
+    props.onSubmit(newApp);
+    console.log(newApp)
+   }
+
+   const handleCancel = (event:React.MouseEvent<HTMLButtonElement>) => {
+      event.preventDefault();
+
+      props.onCancel();
+    
+   }
+    return(
+        <div className="border p-10 fixed inset-0 bg-black/50 z-50">
+            <div className="min-w-lg md:max-w-lg bg-white mx-4 p-8 rounded-lg w-full">
+                {/* Header */}
+                <div>
+                    <h1 className="text-slate-900 text-xl lg:text-2xl">New Job Application</h1>
+                </div>
+                <form className="flex flex-col w-full gap-4 space-x-4 mt-5">
+                    <div>
+                        <label className="text-slate-900 text-lg">Company</label>
+                        <input
+                        name="text"
+                        type="company"
+                        onChange={(e) => setCompany(e.target.value)}
+                        className="w-full border bg-white border-gray-200 text-slate-700 text-base md:text-lg rounded-md px-2 py-1 focus:ring-2 focus:border-blue-600 "
+                        />
+                    </div>
+                    <div className=" ">
+                        <label className="text-slate-900">Location</label>
+                         <input
+                          name="location"
+                        type="location"
+                        onChange={(e) => setLocation(e.target.value)}
+                        className="w-full border bg-white border-gray-200 text-slate-700 text-base md:text-lg rounded-md px-2 py-1 focus:ring-2 focus:border-blue-600 "
+                         />
+                    </div>
+                    <div className="">
+                        <label className="text-slate-900">Position</label>
+                         <input
+                        name="position"
+                        type="position"
+                        onChange={(e) => setPosition(e.target.value)}
+                        className="w-full border bg-white border-gray-200 text-slate-700 text-base md:text-lg rounded-md px-2 py-1  focus:ring-2 focus:border-blue-600 "
+                         />
+                    </div>
+                     <div className="">
+                        <label className="text-slate-900">Salary</label>
+                         <input
+                        name="salary"
+                        type="salary"
+                        onChange={(e) => setSalary(e.target.value)}
+                        className="w-full border bg-white border-gray-200 text-slate-700 text-base md:text-lg rounded-md px-2 py-1  focus:ring-2 focus:border-blue-600 "
+                         />
+                    </div>
+                    <div className="">
+                        <label className="text-slate-900">Status</label>
+                         <select
+                        value={status}
+                        onChange={(e) => setStatus(e.target.value)}
+                        className="w-full bg-slate-200 cursor-pointer rounded-md text-base md:text-lg px-3 py-2 text-slate-700 "
+                         >
+                         <option value="Applied">Applied</option>
+                         <option value="Interview">Interview</option>
+                         <option value="Offer">Offer</option>
+                         <option value="Rejected">Rejected</option>
+                         </select>
+                        
+                        {/* buttons */}
+                        <div className="w-full mt-5 lg:mt-10 flex rounded gap-3  ">
+                            <button
+                            type="submit"
+                            onClick={handleSubmit}
+                            className="bg-blue-500 w-full py-2"
+                            >
+                            Add Application
+                            </button>
+                            <button
+                            type="button"
+                           onClick={handleCancel}
+                            className="bg-gray-300 text-black w-full"
+                            >
+                            Cancel
+                            </button>
+                        </div>
+                        
+                    </div>
+                   
+                </form>
+            </div>
+        </div>
+    )
+}
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,117 @@
+import { useState } from "react";
+import NewAppForm from "../components/newApplicationForm";
 
+interface JobApplication {
+    id: number;
+    company: string;
+    location: string;
+    position: string;
+    salary: string;
+    status: string;
+    appliedDate: string;
+
+}
+
+function getStatusColor(status: string){
+    switch (status){
+        case 'Applied':
+            return "bg-purple-300"
+        case 'Interview':
+            return "bg-blue-300"
+            case 'Offer':
+                return "bg-green-300"
+            case 'Rejected':
+                return "bg-red-300"
+            
+    }
+}
 export default function Dashboard(){
+    const [applications, setApplications] = useState<JobApplication[]>([])
+    const [showForm, setShowForm] = useState(false)
+
+    const handleShowForm = () => {
+        setShowForm(!showForm)
+       
+    }
+    const handleAddApplication = (newApp: JobApplication) => {
+       setApplications([...applications, newApp])
+       setShowForm(false);
+       console.log(newApp)
+    }
+
+    const handleCancel = () => {
+       setShowForm(false);
+    }
+
     return(
-        <div className="flex ">
-            <h1>hello</h1>
+        <div className="w-full min-h-screen">
+           <div className="p-6 max-w-7xl mx-auto space-y-4">
+                <div>
+                    <h1 className="text-xl sm:text-2xl md:text-3xl">Job Applications</h1>
+                </div>
+
+                <div className="border p-3 rounded">
+                    {/* create button */}
+                    <div className="text-right flex gap-4">
+                        <input className="w-full border bg-white border-gray-200 rounded">
+                        </input>
+                        <button onClick={handleShowForm}  
+                            className="border  bg-blue-500 whitespace-nowrap px-2 py-1 "
+                        >   
+                            + New Application
+                        </button>
+                      
+                    </div>
+                </div>
+                        {showForm && <NewAppForm onSubmit={handleAddApplication} onCancel={handleCancel}/>}
+
+                {/* Table  */}
+                <div>
+                    <div>
+                        <table className="w-full border border-gray-300 ">
+                            <thead className="text-left ">
+                                <tr className="text-slate-600 font-extralight bg-neutral-200">
+                                    <th className="p-3">Company</th>
+                                    <th>Position</th>
+                                    <th>Status</th>
+                                    <th>Applied</th>
+                                   
+                                </tr>
+                            </thead>
+                            <tbody className="divide-y divide-gray-300 ">
+                                {applications.map((app)=> (
+                                    <tr key={app.id} className="text-slate-900">
+                                        <td>
+                                            <div className="flex flex-col px-6 py-3 rounded">
+                                                <div>{app.company}</div>
+                                                <div className="text-gray-600">{app.location}</div>
+                                            </div>
+                                            
+                                        </td>
+                                        <td>
+                                            <div className="flex flex-col">
+                                                <div>{app.position}</div>
+                                                <div className="text-gray-600">{app.salary}</div>
+                                            </div>
+                                            
+                                        </td>
+                                        
+                                        <td>
+                                            <span className={`px-3 py-2 rounded-full ${getStatusColor(app.status)}`}>
+                                                {app.status}
+                                            </span>
+                                            
+                                            
+                                        </td>
+                                        <td>{app.appliedDate}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                            
+                        </table>
+                    </div>
+                </div>
+           </div>
         </div>
     )
 }
